d0da68d32be31d5e33d1f3bae0ec96a49c56162d
Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>
2024-08-10 16:45:03 +0200

    Add Module documentation
    Signed-off-by: Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>

--- safe-discriminant/src/lib.rs
+++ safe-discriminant/src/lib.rs
@@ -1,6 +1,36 @@
-
+//! The [Discriminant] trait, along with its corresponding derive macro,
+//! offers a constant-time and safe method for extracting the primitive
+//! form of the discriminant value for enums.
+//!
+//! The trait is derivable only when the following conditions are met:
+//! 1.  The enum's is accompanied by `#[repr(x)]` attribute,
+//!     where `x` must be one of the supported primitive
+//!     types: `u8`, `i8`, `u16`, `i16`, `u32`, `i32`, `u64`,
+//!     or `i64`.
+//!
+//! 2. All enum variants have explicit discriminants.
+//!
+//! # Usage
+//!
+//! To use this macro, simply annotate your enum with `#[derive(Discriminant)]` as follows:
+//!
+//! ```rust
+//! use safe_discriminant::Discriminant;
+//! #[derive(Discriminant)]
+//! #[repr(u8)]
+//! enum Foo {
+//!     A = 1,
+//!     B(u8) = 2,
+//!     C{inner: u8} = 3,
+//! }
+//! let a = Foo::A;
+//! let b = Foo::B(5);
+//! let c = Foo::C{inner: 6};
+//! assert_eq!(a.discriminant(), 1);
+//! assert_eq!(b.discriminant(), 2);
+//! assert_eq!(c.discriminant(), 3);
+//! ```
 pub use safe_discriminant_derive::Discriminant;
-
 /// <div class="warning">
 ///
 /// # Hazmat
@@ -20,7 +50,7 @@ pub use safe_discriminant_derive::Discriminant;
 /// The derive macro should take care of checking that it is always safe to call this function.
 
 pub unsafe trait Discriminant {
-    /// The type of *discriminant* often represented by the type inside
+    /// The type of *discriminant*, it is often represented by the type inside
     /// `#[repr(u*)]` or `#[repr(i*)]`.
     type Selector: Copy;
     /// Returns the discriminant value of enum variant we are using.
