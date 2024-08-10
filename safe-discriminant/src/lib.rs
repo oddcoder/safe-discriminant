605161e8683c8aeaae41b302777283881de1c7a8
Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>
2024-08-10 11:56:45 +0200

    Add Trait Discriminant
    This trait comes with provided implementation and it calculates the
    discriminant of a function.
    
    Signed-off-by: Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>

--- safe-discriminant/src/lib.rs
+++ safe-discriminant/src/lib.rs
@@ -1,3 +1,27 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/// <div class="warning">
+///
+/// # Hazmat
+/// This trait is safe to derive and use but is unsafe to implement manually.
+/// Consider using `#[derive(Discriminant)]` macro instead.
+/// </div>
+///
+/// # Safety
+/// According to
+/// [rust documentation](https://doc.rust-lang.org/std/mem/fn.discriminant.html#accessing-the-numeric-value-of-the-discriminant)
+/// This trait is only safe to implement if the enum has
+/// [primitive representation](https://doc.rust-lang.org/reference/type-layout.html#primitive-representations)
+/// for its discriminant.
+/// That cannot be done for enums using the default representation,
+/// however, as it’s undefined what layout the discriminant has
+/// and where it’s stored — it might not evenbe stored at all!
+/// The derive macro should take care of checking that it is always safe to call this function.
+
+pub unsafe trait Discriminant {
+    /// The type of *discriminant* often represented by the type inside
+    /// `#[repr(u*)]` or `#[repr(i*)]`.
+    type Selector: Copy;
+    /// Returns the discriminant value of enum variant we are using.
+    fn discriminant(&self) -> Self::Selector {
+        unsafe { *<*const _>::from(self).cast() }
+    }
 }
